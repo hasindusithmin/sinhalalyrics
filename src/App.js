3cf2ca76fd7c108b4139daaa12b9040f6170b999
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-15 17:42:01 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import autoComplete from "@tarekraafat/autocomplete.js";
 function App() {
+  const [singers,setSingers] = useState({})
+  const [songs,setSongs] = useState({})
+  useEffect(()=>{
+    fetch('https://sinhalalyrics.deta.dev/singers')
+      .then(res=>res.json())
+      .then(data=>{
+        const _singers = {}
+        for (const {name,key} of data) {
+          _singers[name] = key;
+        }
+        const autoCompleteJS = new autoComplete({
+          selector: "#singers",
+          placeHolder: "Search for singers ...",
+          data: {
+              src: Object.keys(_singers),
+              cache: true,
+          },
+          resultItem: {
+              highlight: true
+          },
+          events: {
+              input: {
+                  selection: (event) => {
+                      const selection = event.detail.selection.value;
+                      autoCompleteJS.input.value = selection;
+                  }
+              }
+          }
+      });
+      setSingers(_singers)
+      })
+      fetch('https://sinhalalyrics.deta.dev/songs')
+      .then(res=>res.json())
+      .then(data=>{
+        const _songs = {}
+        for (const {name,key} of data) {
+          _songs[name] = key;
+        }
+        const autoCompleteJS = new autoComplete({
+          selector: "#songs",
+          placeHolder: "Search for songs ...",
+          data: {
+              src: Object.keys(_songs),
+              cache: true,
+          },
+          resultItem: {
+              highlight: true
+          },
+          events: {
+              input: {
+                  selection: (event) => {
+                      const selection = event.detail.selection.value;
+                      autoCompleteJS.input.value = selection;
+                  }
+              }
+          }
+      });
+      setSongs(_songs)
+      })
+
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input id="singers" type="search" dir="ltr" spellCheck={false} autoCorrect="off" autoComplete="off" autoapitalize="off"/>
+        <input id="songs" type="search" dir="ltr" spellCheck={false} autoCorrect="off" autoComplete="off" autoapitalize="off"/>
+
     </div>
   );
 }
