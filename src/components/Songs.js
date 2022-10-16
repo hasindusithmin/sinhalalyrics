43373d27442e8bddf93f5def118d3fc33de6cda1
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-16 14:23:56 +0000

    Update

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,13 +1,13 @@
 
 
-export default function Songs({handler,songsObj}) {
+export default function Songs({ handler, songsObj }) {
     return (
         <div className="w3-container w3-center w3-white">
-            <ul className="w3-ul w3-hoverable w3-monospace">
-                {Object.entries(songsObj).map(arr=><li onClick={()=>{handler(arr[1])}} key={arr[1]}>{arr[0]}</li>)}
+            <ul className="w3-ul w3-hoverable w3-monospace" id="ul">
+                {Object.entries(songsObj).map(arr => <li onClick={() => { handler(arr[1]) }} key={arr[1]}>{arr[0]}</li>)}
             </ul>
         </div>
     )
-    
+
 
 }
