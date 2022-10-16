957bbf0352322065be0df51ca8f905f37cd39c55
hasindu sithmin <hasindusithmin64@gmail.com>
2022-10-16 17:12:42 +0000

    Update Alphaber.js

--- src/components/Alphaber.js
+++ src/components/Alphaber.js
@@ -1,22 +1,25 @@
 import { useEffect } from "react";
 import alphabet from "../alphabet.json"
 
-function SingersList(singers) {
-    let txt = ''
-    singers.forEach(singer => {
-        txt += `<li key=${Object.values(singer)} >${Object.keys(singer)}</li>\n`
-    })
-    return txt;
-}
-
 
 export default function Alphabet() {
     const alpha = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     let i = 0;
     const render = (i)=>{
         const singers =  alphabet[alpha[i]];
-        const html = SingersList(singers)
-        document.getElementById('ul').innerHTML = html
+        const ul = document.createElement('ul');
+        ul.className = 'w3-ul w3-hoverable w3-monospace'
+        singers.forEach(singer => {
+            const li = document.createElement('li');
+            const id = Object.values(singer)
+            li.id = id
+            li.innerText = Object.keys(singer)
+            li.onclick = ()=>{
+                console.log(id)
+            }
+            ul.appendChild(li)
+        })
+        document.getElementById('singers').appendChild(ul)
     }
     useEffect(()=>{render(0)},[])
     
@@ -28,15 +31,14 @@ export default function Alphabet() {
         i = i !== 25 ? i +1 : 0
         render(i);
     }
+
     return (
         <div className="w3-container">
             <div className="w3-bar">
                 <button className="w3-bar-item w3-button w3-left w3-light-grey" onClick={minus}>« Prev</button>
                 <button className="w3-bar-item w3-button w3-right w3-green" onClick={plus}>Next »</button>
             </div>
-            <div className="w3-container w3-center w3-white">
-                <ul className="w3-ul w3-hoverable w3-monospace" id="ul"></ul>
-            </div>
+            <div className="w3-container w3-center w3-white" id="singers"></div>
         </div>
     )
 }
